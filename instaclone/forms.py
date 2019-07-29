ee6f1b86de73b0ef20b9ddd0d16a1ffa26299e74
Machanga <florenceshiru765@gmail.com>
2019-07-29 05:47:02 +0300

    create the forms that a user will fill while commenting

--- instaclone/forms.py
+++ instaclone/forms.py
@@ -6,3 +6,23 @@ class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['likes', 'userId', 'user']
+
+class EditProfile(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['userId']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comments
+        exclude=['user','images', 'description']
+
+class Likes(forms.ModelForm):
+    class Meta:
+        model=Image
+        exclude=['likes','description','comments','date','user','userId','profile','image','name','caption']
+
+class FollowForm(forms.ModelForm):
+    class Meta:
+        model=Followers
+        exclude=['user','follower']
