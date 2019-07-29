78a04b451e82a0b19452da2a0759911ca4c774a6
Machanga <florenceshiru765@gmail.com>
2019-07-29 06:06:29 +0300

    create the view function urls that will be accessed once a user makes a request

---
+++ instaclone/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns=[
+    url('^$', views.home, name = 'home'),
+    url(r'^upload$', views.upload, name='upload'),
+    url(r'^myprofile$',views.myprofile,name='myprofile'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
