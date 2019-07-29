91f3d8646b9808813b2b7312790dc7c8559d0a5f
Machanga <florenceshiru765@gmail.com>
2019-07-29 11:06:46 +0300

    deployment to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 #    }
 # production
 # else:
-    DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-    }
+DATABASES = {
+'default': dj_database_url.config(
+    default=config('DATABASE_URL')
+)
+}
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
