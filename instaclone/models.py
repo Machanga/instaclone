e7b4ab9041179be3f7525725eee69468ab025659
Machanga <florenceshiru765@gmail.com>
2019-07-29 05:27:41 +0300

    create the image model and class variables that represent different columns in our database.

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    '''
+    image Class for all images added to the app
+    '''
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=300, blank = True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    likes=models.IntegerField(default=0)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    userId=models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-post_date']
